aef92638cf916895a86454428730cad3e0e048f1
danna carreon <dannacarreon@MacBook-Pro-de-danna.local>
2022-10-02 22:26:18 -0500

    Edad

---
+++ U2/03_EdadDelcliente.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+int edad; 
+
+cout<<"Ingrese su edad: ";
+cin>>edad;
+
+if ( edad < 4 ) {
+cout<<"\nPuede entrar gratis! :D"<<endl;
+} else if ( edad >= 4 && edad <= 18 ) {
+cout<<"\nDebe pagar $5 :)"<<endl;
+} else {
+cout<<"\nDebe pagar $10 :)"<<endl;
+}
